c59500cd0a3aacb5ab74e8a686bf168460591cd5
Lokesh Punwani <113304906+Lokeshpunwani29@users.noreply.github.com>
2025-03-28 23:44:37 +0530

    Updated Logic

--- ArrayWithSpace.java
+++ ArrayWithSpace.java
@@ -5,6 +5,12 @@ public class ArrayWithSpace
 		
 		Scanner input = new Scanner(System.in);
 		String StringInput = input.nextLine();
+
+		// if input is like {1,2,3,4,5,6,7,8,9,10}
+		if(StringInput.startsWith("{") && StringInput.endsWith("}"))
+		{
+		   StringInput = StringInput.substring(1,StringInput.length()-1); 
+		}
 		
 		String[] CharArray = StringInput.split(" "); // Split by anything like: "," "-"
 		
