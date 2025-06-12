3ba4d90d867b303a3b7f3a5bd4b94f2dd6886e26
Lokesh Punwani <113304906+Lokeshpunwani29@users.noreply.github.com>
2025-06-13 00:41:25 +0530

    Update SortingAlgos.java

--- SortingAlgos.java
+++ SortingAlgos.java
@@ -1,5 +1,20 @@
 public class SortingAlgos
 {
+	static void insertionSort(int[] arr)
+	{
+		int n = arr.length;
+		for(int i=1; i<n; i++)      // supposing arr[0] is sorted
+		{
+            int current = arr[i];   // starting of unsorted arr
+            int j = i-1;            // keep track of sorted arr
+            while( j >= 0 && current < arr[j])      // current is compared with its left value
+            {
+                arr[j+1] = arr[j];      // pushing larger elements at left towards right
+                j--;                    // decreasing to compare with more left values
+            }
+            arr[j+1] = current;         // placing current at correct position
+		}
+	}
 	static void selectionSort(int[] arr)
 	{
 		int temp;
@@ -11,9 +26,9 @@ public class SortingAlgos
 			{
 				if(arr[j] < arr[smallestIdx]) smallestIdx = j;  // smallestIdx found
 			}
-            temp = arr[i];              // swap
-            arr[i] = arr[smallestIdx];  // smallest value goes to sorted part which is at ith Index which
-            arr[smallestIdx] = temp;    // considered to be start of unsorted array.
+			temp = arr[i];              // swap
+			arr[i] = arr[smallestIdx];  // smallest value goes to sorted part which is at ith Index which
+			arr[smallestIdx] = temp;    // considered to be start of unsorted array.
 		}
 	}
 	static void bubbleSort(int[] arr)
@@ -44,7 +59,8 @@ public class SortingAlgos
 		for(int a:arr) System.out.print(a+" ");
 
 		//bubbleSort(arr);
-		selectionSort(arr);
+		//selectionSort(arr);
+		insertionSort(arr);
 		System.out.println("\nAfter Sorting:");
 		for(int a:arr) System.out.print(a+" ");
 	}
